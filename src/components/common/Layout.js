402a9ff46be8588ff801695331a19790d8726d94
Nilotpal Roychowdhury <nilotpal.roy05@gmail.com>
2020-12-09 20:28:00 +0530

    Removed Published with Ghost text

--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -81,9 +81,9 @@ const DefaultLayout = ({ data, children, bodyClass, isHome }) => {
                     {/* The footer at the very bottom of the screen */}
                     <footer className="site-foot">
                         <div className="site-foot-nav container">
-                            <div className="site-foot-nav-left">
+                            {/* <div className="site-foot-nav-left">
                                 <Link to="/">{site.title}</Link> © 2019 &mdash; Published with <a className="site-foot-nav-item" href="https://ghost.org" target="_blank" rel="noopener noreferrer">Ghost</a>
-                            </div>
+                            </div> */}
                             <div className="site-foot-nav-right">
                                 <Navigation data={site.navigation} navClass="site-foot-nav-item" />
                             </div>
